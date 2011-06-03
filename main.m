956b5d2efe20760320337cf10f35d6b9da17d575
Pablo Merino <pablo.perso1995@gmail.com>
2011-06-03 22:36:16 +0200

    Main files

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pablo
+//
+//  Created by Pablo Merino on 01/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
